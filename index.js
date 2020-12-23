9519af5528bf42549643b394d5fc784b9af7babf
ArianKG <72304422+ArianKG@users.noreply.github.com>
2020-12-23 11:10:17 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const p = document.querySelector('.p');
+p.id = document.querySelector('p');
+
 setInterval(
 function randomUnicode() {
     var txt = String.fromCharCode(Math.floor(Math.random() * 65535));
