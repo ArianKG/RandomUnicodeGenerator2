8c1d9ec634c4c6f7f078dc9b8ff6fcd3890f6233
ArianKG <72304422+ArianKG@users.noreply.github.com>
2021-01-08 16:42:13 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const p = document.querySelector('.p');
 
 setInterval(
 function randomUnicode() {
-    var txt = String.fromCharCode(Math.floor(Math.random() * 65535));
-    p.innerHTML = txt;
+    var unicode = String.fromCharCode(Math.floor(Math.random() * 65535));
+    p.innerHTML = unicode;
 }, 1000)
